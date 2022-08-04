76a69b470850f29cad4ab9ba8ffd2ab7e85c41d1
Wtyka <76787469+Wtyka2504@users.noreply.github.com>
2022-08-04 14:46:02 +0200

    Files

---
+++ scripts/56b510.js
@@ -0,0 +1,9 @@
+(()=>{"use strict";var e={"./styles/index.scss":
+/*!***************************!*\
+  !*** ./styles/index.scss ***!
+  \***************************/(e,t,n)=>{n.r(t)}},t={};function n(r){var o=t[r];if(void 0!==o)return o.exports;var s=t[r]={exports:{}};return e[r](s,s.exports,n),s.exports}n.r=e=>{"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})};var r={};(()=>{
+/*!**************************!*\
+  !*** ./scripts/index.js ***!
+  \**************************/
+n.r(r);var e;n(/*! ../styles/index.scss */"./styles/index.scss");e=document.getElementById("navbar"),document.addEventListener("DOMContentLoaded",(function(){setTimeout((function(){document.documentElement.style.setProperty("--navbar-height","".concat(e.clientHeight,"px"))}),1)}))})()})();
+//# sourceMappingURL=56b510.js.map
